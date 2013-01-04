efcda00e952c55e41acd94cee4b866b1bb3aad99
W. Trevor King <wking@tremily.us>
2013-01-04 07:21:19 -0500

    utils: Add docstring and shutil namespaces to copytree
    Now the new function duplicates shutil.copytree exactly.

--- pelican/utils.py
+++ pelican/utils.py
@@ -339,6 +339,17 @@ def mkdir_p(path):
 
 
 def copytree(src, dst, symlinks=False, ignore=None):
+    """shutil.copytree clone
+
+    :param src: the source dir (from shutil.copytree)
+    :param dst: the destination dir (from shutil.copytree)
+    :param symlinks: preserve symlinks (from shutil.copytree)
+    :param ignore: filtering callable (from shutil.copytree)
+
+    Implemenation based on the copytree example:
+
+      http://docs.python.org/2/library/shutil.html#copytree-example
+    """
     names = os.listdir(src)
     if ignore is not None:
         ignored_names = ignore(src, names)
@@ -359,20 +370,20 @@ def copytree(src, dst, symlinks=False, ignore=None):
             elif os.path.isdir(srcname):
                 copytree(srcname, dstname, symlinks, ignore)
             else:
-                copy2(srcname, dstname)
+                shutil.copy2(srcname, dstname)
             # XXX What about devices, sockets etc.?
         except (IOError, os.error) as why:
             errors.append((srcname, dstname, str(why)))
         # catch the Error from the recursive copytree so that we can
         # continue with other files
-        except Error as err:
+        except shutil.Error as err:
             errors.extend(err.args[0])
     try:
-        copystat(src, dst)
+        shutil.copystat(src, dst)
     except WindowsError:
         # can't copy file access times on Windows
         pass
     except OSError as why:
         errors.extend((src, dst, str(why)))
     if errors:
-        raise Error(errors)
+        raise shutil.Error(errors)
